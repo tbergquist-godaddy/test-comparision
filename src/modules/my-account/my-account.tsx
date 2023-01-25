3e7abfdee2f8e9bbffa1d2ddd0b439bacfa1b12d
Trond Bergquist <tbergquist@godaddy.com>
2023-01-25 10:57:28 -0500

    enable strict null checks

--- src/modules/my-account/my-account.tsx
+++ src/modules/my-account/my-account.tsx
@@ -2,5 +2,5 @@ import useAccount from './hooks/useAccount';
 
 export default function MyAccount(): JSX.Element {
   const { data } = useAccount();
-  return <h1>Hello {data.name}</h1>;
+  return <h1>Hello {data?.name}</h1>;
 }
